fd2765250889c820bfadcdd777b83e423f0dddd1
Hassan M Sabir <hassan.ciit045@gmail.com>
2023-04-16 05:43:33 +0500

    addition 1

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -12,18 +12,19 @@ const Home = () => {
         </div>
         <div className="services-section">
           <h2>Our Services</h2>
-          <hr/>
+          <hr className='heading-hr'/>
           <div className="services">
 
-          <div className='service-box box1'>
+          <div className='service-box box1 p-3 w-50 w-sm-75 w-md-50 w-lg-25'>
             <div className="service-heading">
               Exam sittting arrangement
             </div>
+            <hr />
             <div className="searvice-desc">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut deserunt, veritatis blanditiis, assumenda impedit vitae laborum velit, dolor enim commodi praesentium deleniti eligendi repudiandae nemo necessitatibus a magni architecto at!
             </div>
           </div>
-          <div className='service-box box2'>
+          <div className='service-box box2 p-3 w-50 w-sm-75 w-md-50 w-lg-25'>
             <div className="service-heading">
               Exam sittting arrangement 2
             </div>
