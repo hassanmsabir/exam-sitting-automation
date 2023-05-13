76309a1a447b38ae9adc7a0b9f1be0154463ff66
admin <admin@gmail.com>
2023-05-14 04:09:15 +0500

    more

--- src/app/pages/Schedule.js
+++ src/app/pages/Schedule.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { actionAPI, useSharedDispatcher, useSharedSelector } from "../shared";
-import { Modal } from "antd";
+import { Modal, Spin } from "antd";
 import SeatingArrangementTable from "../components/TeacherView/SeatingArrangementTable";
 
 const Schedule = () => {
-  const { ExamSchedulesListing, ExamSchedulesListingSuccess } =
-    useSharedSelector((state) => state.ListAllExamSchedules);
+  const {
+    ExamSchedulesListing,
+    ExamSchedulesListingLoading,
+    ExamSchedulesListingSuccess,
+  } = useSharedSelector((state) => state.ListAllExamSchedules);
   const [totalStudents, setTotalStudents] = useState(0);
   const [isViewSeatingPlanModalOpen, setIsViewSeatingPlanModalOpen] =
     useState(false);
@@ -24,75 +27,82 @@ const Schedule = () => {
     <>
       <h3>View The Scheduled Exams here</h3>
       <hr />
-      {ExamSchedulesListing ? (
-        ExamSchedulesListing?.entry?.map((item) => {
-          return (
-            <div className="exam-card">
-              <div className="card-header bg-success d-flex flex-wrap justify-content-between p-2">
-                <p className="exam-name text-light fs-6 fw-bold m-0">
-                  {item?.examName}
-                </p>
-                <p className="exam-date text-light fs-6 fw-bold m-0">
-                  {item?.examDate} - {item?.examTime}
-                </p>
+      {!ExamSchedulesListingLoading ? (
+        ExamSchedulesListing ? (
+          ExamSchedulesListing?.entry?.map((item) => {
+            return (
+              <div className="exam-card my-3">
+                <div className="card-header bg-success d-flex flex-wrap justify-content-between p-2">
+                  <p className="exam-name text-light fs-6 fw-bold m-0">
+                    {item?.examName}
+                  </p>
+                  <p className="exam-date text-light fs-6 fw-bold m-0">
+                    {item?.examDate} - {item?.examTime}
+                  </p>
+                </div>
+                <div className="card-body d-flex flex-wrap justify-content-between bg-light p-2">
+                  <div className="card-body-right">
+                    <p className="">
+                      <span className="fw-bold me-1">Teacher:</span>
+                      <span className="fs-6 me-1">
+                        {item?.classesData?.map(
+                          (classData) => `${classData?.teacherName}, `
+                        )}
+                      </span>
+                    </p>
+                    <p className="">
+                      <span className="fw-bold me-1">Class/es:</span>
+                      <span className="fs-6 me-1">
+                        {item?.classesData?.map(
+                          (classData) => `${classData?.batchName}, `
+                        )}
+                      </span>
+                    </p>
+                    <p className="">
+                      <span className="fw-bold me-1">Student/s:</span>
+                      <span className="fs-6 me-1">
+                        {item?.classesData?.map(
+                          (classData) => `${classData?.batchName}, `
+                        )}
+                      </span>
+                    </p>
+                  </div>
+                  <div className="card-body-center">
+                    <p className="">
+                      <span className="fw-bold me-1">Seats:</span>
+                    </p>
+                    <p className="">
+                      <span className="fw-bold me-1">Room:</span>
+                      <span className="fs-6 me-1">{item?.hallName}</span>
+                    </p>
+                  </div>
+                  <div className="card-body-left">
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => {
+                        setIsViewSeatingPlanModalOpen(true);
+                        setSelectedSeatingPlan(item?.seatingArrangement);
+                      }}
+                    >
+                      View
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div className="card-body d-flex flex-wrap justify-content-between bg-light p-2">
-                <div className="card-body-right">
-                  <p className="">
-                    <span className="fw-bold me-1">Teacher:</span>
-                    <span className="fs-6 me-1">
-                      {item?.classesData?.map(
-                        (classData) => `${classData?.teacherName}, `
-                      )}
-                    </span>
-                  </p>
-                  <p className="">
-                    <span className="fw-bold me-1">Class/es:</span>
-                    <span className="fs-6 me-1">
-                      {item?.classesData?.map(
-                        (classData) => `${classData?.batchName}, `
-                      )}
-                    </span>
-                  </p>
-                  <p className="">
-                    <span className="fw-bold me-1">Student/s:</span>
-                    <span className="fs-6 me-1">
-                      {item?.classesData?.map(
-                        (classData) => `${classData?.batchName}, `
-                      )}
-                    </span>
-                  </p>
-                </div>
-                <div className="card-body-center">
-                  <p className="">
-                    <span className="fw-bold me-1">Seats:</span>
-                  </p>
-                  <p className="">
-                    <span className="fw-bold me-1">Room:</span>
-                    <span className="fs-6 me-1">{item?.hallName}</span>
-                  </p>
-                </div>
-                <div className="card-body-left">
-                  <button
-                    className="btn btn-primary"
-                    onClick={() => {
-                      setIsViewSeatingPlanModalOpen(true);
-                      setSelectedSeatingPlan(item?.seatingArrangement);
-                    }}
-                  >
-                    View
-                  </button>
-                </div>
-              </div>
-            </div>
-          );
-        })
+            );
+          })
+        ) : (
+          <p>No Exam Schedule Data</p>
+        )
       ) : (
-        <p>No Exam Schedule Data</p>
+        <div className="d-flex justify-content-center p-4">
+          <Spin />
+        </div>
       )}
       <Modal
         open={isViewSeatingPlanModalOpen}
         onOk={() => setIsViewSeatingPlanModalOpen(false)}
+        onCancel={() => setIsViewSeatingPlanModalOpen(false)}
         // cancelButtonProps={{ style: { display: "none" } }}
         title="View Seating Plan"
         width={1000}
